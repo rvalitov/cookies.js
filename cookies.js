0f1e43b992f2995898fa1b166c542772135c929f
Ramil Valitov <ramilvalitov@gmail.com>
2018-10-30 20:54:47 +0300

    [fix] remove reduntant escape characters

--- cookies.js
+++ cookies.js
@@ -27,10 +27,10 @@
 var docCookies = {
 	getItem: function (sKey) {
 		if (!sKey) { return null; }
-		return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+		return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-.+*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
 	},
 	setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
-		if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+		if (!sKey || /^(?:expires|max-age|path|domain|secure)$/i.test(sKey)) { return false; }
 		var sExpires = "";
 		if (vEnd) {
 			switch (vEnd.constructor) {
@@ -63,11 +63,11 @@ var docCookies = {
 		return true;
 	},
 	hasItem: function (sKey) {
-		if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
-		return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+		if (!sKey || /^(?:expires|max-age|path|domain|secure)$/i.test(sKey)) { return false; }
+		return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-.+*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
 	},
 	keys: function () {
-		var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+		var aKeys = document.cookie.replace(/((?:^|\s*;)[^=]+)(?=;|$)|^\s*|\s*(?:=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:=[^;]*)?;\s*/);
 		for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) {
 			aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]);
 		}
